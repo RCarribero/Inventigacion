61ebb6f43b15d5dedc8b38578b976e10e0e4ad88
Ruben Carribero Garcia <rcarribero@proton.me>
2024-12-25 23:36:31 +0100

    .

--- Ventas/src/Ventas.java
+++ Ventas/src/Ventas.java
@@ -3,20 +3,20 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Ventas {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Producto> lista = new ArrayList<Producto>();
         String decision;
-        int id=1;
+        int id = 1;
         Scanner input = new Scanner(System.in);
         do {
-                System.out.println("¿Desea agregar un nuevo producto?");
-                decision=input.nextLine();
-                if (decision.equalsIgnoreCase("Si")) {
-                    Producto.agregarProducto(lista, input,id);
-                    id++;
-                }
-            } while (!decision.equalsIgnoreCase("no"));
-            Producto.calcularSuma(lista);
-            Producto.orden(lista);
-        }    
+            System.out.println("¿Desea agregar un nuevo producto?");
+            decision = input.nextLine();
+            if (decision.equalsIgnoreCase("Si")) {
+                Producto.agregarProducto(lista, input, id);
+                id++;
+            }
+        } while (!decision.equalsIgnoreCase("no"));
+        Producto.calcularSuma(lista);
+        Producto.orden(lista);
+    }
 }
